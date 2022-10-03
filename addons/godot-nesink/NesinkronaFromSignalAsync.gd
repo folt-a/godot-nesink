9d08babd790929a234f0c4bca755ccb2d8615267
ydicoder <79189798+ydipeepo@users.noreply.github.com>
2022-10-03 10:24:28 +0900

    Update NesinkronaFromSignalAsync.gd

--- addons/godot-nesink/NesinkronaFromSignalAsync.gd
+++ addons/godot-nesink/NesinkronaFromSignalAsync.gd
@@ -14,13 +14,27 @@ func _init(
 
 	assert(signal_argc <= MAX_SIGNAL_ARGC)
 
-	match signal_argc:
-		0: signal_.connect(_on_completed_0, CONNECT_ONE_SHOT)
-		1: signal_.connect(_on_completed_1, CONNECT_ONE_SHOT)
-		2: signal_.connect(_on_completed_2, CONNECT_ONE_SHOT)
-		3: signal_.connect(_on_completed_3, CONNECT_ONE_SHOT)
-		4: signal_.connect(_on_completed_4, CONNECT_ONE_SHOT)
-		5: signal_.connect(_on_completed_5, CONNECT_ONE_SHOT)
+	# https://github.com/ydipeepo/godot-nesink/issues/8
+	# @folt-a さんから報告頂いた嵌りパターン
+	# Async.from_signal(get_tree().create_timer(1.0).timeout) みたくやると
+	# signal_.get_object() がここに到達した段階で <Freed Object> になってる
+	if is_instance_valid(signal_.get_object()):
+		match signal_argc:
+			0: signal_.connect(_on_completed_0, CONNECT_ONE_SHOT)
+			1: signal_.connect(_on_completed_1, CONNECT_ONE_SHOT)
+			2: signal_.connect(_on_completed_2, CONNECT_ONE_SHOT)
+			3: signal_.connect(_on_completed_3, CONNECT_ONE_SHOT)
+			4: signal_.connect(_on_completed_4, CONNECT_ONE_SHOT)
+			5: signal_.connect(_on_completed_5, CONNECT_ONE_SHOT)
+	else:
+		assert(not signal_.is_null())
+		match signal_argc:
+			0: signal_.connect(_on_completed_0)
+			1: signal_.connect(_on_completed_1)
+			2: signal_.connect(_on_completed_2)
+			3: signal_.connect(_on_completed_3)
+			4: signal_.connect(_on_completed_4)
+			5: signal_.connect(_on_completed_5)
 
 func _on_completed_0() -> void:
 	if is_pending:
